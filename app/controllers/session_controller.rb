d8935ea5cc92723b96cc366776641e297445b91a
ReginaF2012 <skritschy@gmail.com>
2020-06-01 13:37:55 -0500

    created login form and post '/login' route

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,12 +4,17 @@ class SessionController < ApplicationController
     end
 
     post '/signup' do
-        @user = User.create(params[:user])
-        session[:user_id] = @user.id
+        user = User.create(params[:user])
+        session[:message] = "Verify Your Information!"
         redirect to '/login'
     end
 
     get '/login' do
+        @message = session[:message]
         erb :'/sessions/login'
+    end
+
+    post '/login' do
+        "Hello World!"
     end
 end
