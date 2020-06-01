0487cdf44381e7163dbc78d0186b6c15d90679e2
ReginaF2012 <skritschy@gmail.com>
2020-06-01 13:24:19 -0500

    set post route in signup form to '/signup', created '/login' route

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,4 +2,14 @@ class SessionController < ApplicationController
     get '/signup' do 
         erb :'sessions/signup'
     end
+
+    post '/signup' do
+        @user = User.create(params[:user])
+        session[:user_id] = @user.id
+        redirect to '/login'
+    end
+
+    get '/login' do
+        erb :'/sessions/login'
+    end
 end
