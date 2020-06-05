780a5b790e6d0883d42a6aa4ffe0063af4770434
ReginaF2012 <skritschy@gmail.com>
2020-06-05 12:00:16 -0500

    removed username column from users table, going to use email instead

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200605165604) do
+ActiveRecord::Schema.define(version: 20200605165927) do
 
   create_table "pet_todos", force: :cascade do |t|
     t.integer "pet_id"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20200605165604) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "password_digest"
     t.string "name"
     t.string "email"
