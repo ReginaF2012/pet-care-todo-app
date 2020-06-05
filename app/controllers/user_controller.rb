ea3421a677726135623636a61d9e3b0edbb128d7
ReginaF2012 <skritschy@gmail.com>
2020-06-05 15:24:31 -0500

    informs the user of overdue todos on homepage, fixed missing .each in overdue.erb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,7 @@ class UserController < ApplicationController
           @user = User.find_by(id: session[:user_id]) if session[:user_id]
           todos = @user.todos.uniq
           @upcoming_todos = todos.select{ |todo| todo.datetime >= Date.today && todo.datetime <= 1.week.from_now && todo.  complete == false}
+          @overdue_todos = current_user.todos.select{ |todo| todo.complete == false && todo.datetime < DateTime.now }
           erb :'users/homepage'
         else
             session[:message] = "You don't have access to that page"
