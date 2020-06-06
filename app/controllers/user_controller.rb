5481f575b60745f168abac59634cd8985db0e84f
ReginaF2012 <skritschy@gmail.com>
2020-06-06 14:08:11 -0500

    wishes a happy birthday/gotchaday on users homepage when it's their birthday

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,10 +2,12 @@ class UserController < ApplicationController
     get '/users/:id' do
         #users cannot see other user's homepage
         if logged_in? && current_user.id == params[:id].to_i
-          @user = User.find_by(id: session[:user_id]) if session[:user_id]
+          @user = User.find_by(id: current_user.id)
           todos = @user.todos.uniq
           @upcoming_todos = todos.select{ |todo| todo.datetime >= Date.today && todo.datetime <= 1.week.from_now && todo.complete == false}
           @overdue_todos = current_user.todos.select{ |todo| todo.complete == false && todo.datetime < DateTime.now }
+          @pets_birthday_today = current_user.pets.select{ |pet| pet.birthday.day == Date.today.day && pet.birthday.month == Date.today.month }
+          @pets_birthday_today_names = @pets_birthday_today.map(&:name).join(" and ")
 
           erb :'users/homepage'
         else
