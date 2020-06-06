2b033cf5d3dd0ee04a27066e8c16affde426a739
ReginaF2012 <skritschy@gmail.com>
2020-06-06 13:05:50 -0500

    got rid of ordering by datetime because if there was only 1 item in the list it would break

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,9 +4,9 @@ class UserController < ApplicationController
         if logged_in? && current_user.id == params[:id].to_i
           @user = User.find_by(id: session[:user_id]) if session[:user_id]
           todos = @user.todos.uniq
-          @upcoming_todos = todos.select{ |todo| todo.datetime >= Date.today && todo.datetime <= 1.week.from_now && todo.  complete == false}
+          @upcoming_todos = todos.select{ |todo| todo.datetime >= Date.today && todo.datetime <= 1.week.from_now && todo.complete == false}
           @overdue_todos = current_user.todos.select{ |todo| todo.complete == false && todo.datetime < DateTime.now }
-          
+
           erb :'users/homepage'
         else
             session[:message] = "You don't have access to that page"
