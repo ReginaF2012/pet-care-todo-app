c98f44e40489ae19a84ec356b80117893a7cef2f
ReginaF2012 <skritschy@gmail.com>
2020-06-06 12:50:41 -0500

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,30 @@
     User.create(email: Faker::Internet.unique.email(name: name, separators: '_'), password: "password", name: name)
 end
 
-30.times do
+5.times do
     Pet.create(name: Faker::Books::Lovecraft.deity, species: Faker::Creature::Animal.name, birthday: Faker::Date.between(from: 5.year.ago, to: Date.today), user: User.all.shuffle.first)
 end
+
+5.times do
+    Pet.create(name: Faker::Ancient.god, species: Faker::Creature::Animal.name, birthday: Faker::Date.between(from: 5.year.ago, to: Date.today), user: User.all.shuffle.first)
+end
+
+5.times do
+    Pet.create(name: Faker::Games::SuperSmashBros.fighter, species: Faker::Creature::Animal.name, birthday: Faker::Date.between(from: 5.year.ago, to: Date.today), user: User.all.shuffle.first)
+end
+
+5.times do
+    Pet.create(name: Faker::Games::Zelda.character, species: Faker::Creature::Animal.name, birthday: Faker::Date.between(from: 5.year.ago, to: Date.today), user: User.all.shuffle.first)
+end
+
+5.times do
+    Pet.create(name: Faker::Games::Fallout., species: Faker::Creature::Animal.name, birthday: Faker::Date.between(from: 5.year.ago, to: Date.today), user: User.all.shuffle.first)
+end
+
+5.times do
+    Pet.create(name: Faker::FunnyName.name, species: Faker::Creature::Animal.name, birthday: Faker::Date.between(from: 5.year.ago, to: Date.today), user: User.all.shuffle.first)
+end
+
 
 100.times do
     #trying to simulate how it would actually work using the app. A user must first be created, and then must have pets before creating todo list items.
@@ -23,5 +44,5 @@ end
         pets_array << user.pets[i]
         i += 1
     end
-    Todo.create(name: Faker::Lorem.sentence, description: Faker::Lorem.paragraphs[0], datetime: rand(30.days).seconds.from_now, complete: Faker::Boolean.boolean, pets: pets_array)
+    Todo.create(name: "#{Faker::Verb.base} #{RandomWord.nouns.next.gsub("_"," ")}".titleize, description: Faker::Lorem.paragraphs[0], datetime: Faker::Time.between(from: Faker::Time.backward(days: 7), to: Faker::Time.forward(days: 30)), complete: Faker::Boolean.boolean, pets: pets_array)
 end
