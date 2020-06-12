274e2b0e0b7662e29c484d35e185c169115a82af
ReginaF2012 <skritschy@gmail.com>
2020-06-11 23:46:36 -0500

    destroys a todo after a pet is destroyed, if that pet was the only pet belonging to that todo

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,6 +4,13 @@ class Pet < ActiveRecord::Base
     belongs_to :user
     has_many :pet_todos
     has_many :todos, through: :pet_todos
+    after_destroy :delete_todos_if_only_pet
+
+    def delete_todos_if_only_pet
+        todos.each do |todo|
+            todo.destroy if todo.pets.count == 0
+        end
+    end
 
     def make_slug
         self.update(slug: "#{self.name.parameterize}#{self.id}")
